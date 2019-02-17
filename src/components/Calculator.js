d6005ef6ca4d736459a3ae133281ff7ca059c40d
Marsel <marsel.kadroff@gmail.com>
2019-02-17 23:51:50 +0500

    add css

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import InputRange from "react-input-range";
-import Display from "./Display";
 
 import "../styles/App.css";
 import "react-input-range/lib/css/index.css";
@@ -12,12 +11,22 @@ class Calculator extends Component {
       amountValue: 500000,
       yearsValue: 36,
       rateValue: 15,
-      payment: 0
+      payment: 0,
+      amountStep: 1000,
+      rateStep: 0.5,
+      paymentStep: 0
     };
   }
 
   handleAmountChange = value => {
     this.setState({ amountValue: value });
+    let resultStep = 0;
+    if (value < 100000) {
+      resultStep = 10000;
+    } else if (value < 1000000) {
+      resultStep = 50000;
+    } else resultStep = 100000;
+    this.setState({ amountStep: resultStep });
   };
   handleYearChange = value => {
     this.setState({ yearsValue: value });
@@ -27,11 +36,17 @@ class Calculator extends Component {
   };
   handelPaymentChange = value => {
     let { yearsValue, rateValue } = this.state;
+    let paymentStep = 0;
     rateValue /= 1200;
     const summa = Math.round(
       (value * (1 - Math.pow(1 + rateValue, -yearsValue))) / rateValue
     );
-    this.setState({ amountValue: summa });
+    if (value < 4000) {
+      paymentStep = 533;
+    } else if (value >= 170000) {
+      paymentStep = 3327;
+    } else paymentStep = 1000;
+    this.setState({ amountValue: summa, paymentStep: paymentStep });
   };
 
   payment = () => {
@@ -44,43 +59,49 @@ class Calculator extends Component {
   };
 
   render() {
-    let { amountValue, yearsValue, rateValue } = this.state;
+    let { amountValue, yearsValue, rateValue, amountStep } = this.state;
 
-    console.log(this.state.payment);
     return (
       <div className="App">
-        <h4>Сумма займа: {amountValue}₽</h4>
-        <InputRange
-          step={100}
-          maxValue={5000000}
-          minValue={100000}
-          value={amountValue}
-          onChange={this.handleAmountChange}
-        />
-        <h4>Срок займа: {yearsValue}м</h4>
-        <InputRange
-          step={1}
-          maxValue={240}
-          minValue={1}
-          value={yearsValue}
-          onChange={this.handleYearChange}
-        />
-        <h4>Ставка{rateValue}%</h4>
-        <InputRange
-          step={0.5}
-          maxValue={60}
-          minValue={15}
-          value={rateValue}
-          onChange={this.handleRateChange}
-        />
-        <h4>Выплата {this.payment()}₽</h4>
-        <InputRange
-          step={1000}
-          maxValue={200000}
-          minValue={15}
-          value={this.payment()}
-          onChange={this.handelPaymentChange}
-        />
+        <div className="left-slider">
+          <h4>Сумма займа: {amountValue}₽</h4>
+          <InputRange
+            step={amountStep}
+            maxValue={5000000}
+            minValue={100000}
+            value={amountValue}
+            onChange={this.handleAmountChange}
+          />
+          <h4>Срок займа: {yearsValue}м</h4>
+          <InputRange
+            step={1}
+            maxValue={240}
+            minValue={1}
+            value={yearsValue}
+            onChange={this.handleYearChange}
+            className="input_range"
+          />
+          <h4>Ставка{rateValue}%</h4>
+          <InputRange
+            step={0.5}
+            maxValue={60}
+            minValue={15}
+            value={rateValue}
+            onChange={this.handleRateChange}
+            className="input_range"
+          />
+        </div>
+        <div className="right-slider">
+          <h4>Выплата {this.payment()}₽</h4>
+          <InputRange
+            step={1000}
+            maxValue={200000}
+            minValue={15}
+            value={this.payment()}
+            onChange={this.handelPaymentChange}
+            className="input_range"
+          />
+        </div>
         {/* <Display years={yearsValue} amount={amountValue} /> */}
       </div>
     );
